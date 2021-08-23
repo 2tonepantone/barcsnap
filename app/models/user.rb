1eabc0bdc6492445bd9101b58612a687a87a9703
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-23 17:17:05 +0900

    add photo and display_name to user and devise forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :reviews
+  has_one_attached :photo
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :display_name, length: { within: 5..12 }
 end
