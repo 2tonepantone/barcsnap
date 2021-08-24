01a6dee6e2ac2fe886372bf4dc1a6007cb384605
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-24 16:21:00 +0900

    import ScanditSDK JS plugin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import * as ScanditSDK from "scandit-sdk";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -27,6 +28,7 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  ScanditSDK();
 });
 
 import "controllers"
