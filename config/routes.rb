7eee476488d2574150574dee8e2f53a665897aba
kochapas <kochapas@outlook.com>
2021-08-25 11:27:46 +0900

    Add route to ui_kits page for debugging purpose

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/ui_kits', to: 'pages#ui_kits'
   resources :products, only: [:show] do
     resources :favorites, only: %i[create destroy]
     resources :reviews, only: [:create]
