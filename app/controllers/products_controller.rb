e769880991c6929a9b45fb99f703d19774ff3a4b
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-25 16:59:04 +0900

    implement barcode scan functionality and remove user authentication from said scan

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,27 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[show create]
+
   def show
     @product = Product.find(params[:id])
   end
+
+  def create
+    barcode = params[:barcode]
+    jancode_scraper = ScrapeJancodeService.new(barcode)
+    jancode_scraper.call
+    @product = jancode_scraper.create_product
+
+    if @product.save
+      redirect_to product_path(@product), notice: 'Barcode was scanned successfully.'
+    else
+      redirect_to root_path, alert: "Failed to scan barcode. #{@product.errors.full_messages.join(', ')}."
+    end
+  end
+
+  # private
+
+  # def product_params
+  #   params.require(:product).permit(:name, :barcode, :company_name,
+  #                                   :ingredients, :size, :photo, :reviews, :tags)
+  # end
 end
