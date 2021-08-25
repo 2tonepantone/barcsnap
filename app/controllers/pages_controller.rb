db5805a8d9e1aef5c2960322a8c221861795024a
kochapas <kochapas@outlook.com>
2021-08-25 16:00:31 +0900

    Add home page and popular reviews area design.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :ui_kits ]
 
   def home
+    @top_rating_products = Review.order(rating: :desc).map(&:product).uniq
   end
 end
