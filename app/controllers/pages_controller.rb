390dae5e026778c23e1bba6913837292e77331c9
kochapas <kochapas@outlook.com>
2021-08-25 17:00:17 +0900

    Add most liked cards to home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :ui_kits ]
 
   def home
-    @top_rating_products = Review.order(rating: :desc).map(&:product).uniq
+    @top_rating_products = Review.order(rating: :desc).map(&:product).uniq[0..3]
+    @most_liked_products = Product.all.sort_by { |p| p.favoritors.count }.reverse[0..3]
   end
 end
