6ffd32beff32f0e7c91e66703d3c4cdf00ddcfbd
kochapas <kochapas@outlook.com>
2021-08-26 19:36:10 +0900

    User can toggle favorite on the product.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,10 @@
 class FavoritesController < ApplicationController
   def create
-    product = Product.find(params[:id])
-    current_user.favorite(product)
+    product = Product.find(params[:product_id])
+    if current_user.favorited?(product) == false
+      current_user.favorite(product)
+    else
+      current_user.unfavorite(product)
+    end
   end
 end
