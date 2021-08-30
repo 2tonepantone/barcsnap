d01ee99214a8635354b3b39e0d11f7f31081b5c6
kochapas <kochapas@outlook.com>
2021-08-30 15:10:26 +0900

    Adjust responsiveness of grid to 3 levels.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :ui_kits ]
 
   def home
-    @top_rating_products = Product.all.sort_by { |p| p.avg_rating || 0 }.reverse[0..3]
-    @most_liked_products = Product.all.sort_by { |p| p.favoritors.count }.reverse[0..3]
+    @top_rating_products = Product.all.sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
+    @most_liked_products = Product.all.sort_by { |p| p.favoritors.count }.reverse[0..5]
   end
 end
