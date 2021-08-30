a3da0968e6f614394a31ac51d5c4c0709f9ea29a
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-30 17:33:56 +0900

    add allergens method to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,11 @@ class Product < ApplicationRecord
   acts_as_taggable_on :tags
   ActsAsTaggableOn.force_lowercase = true
   acts_as_favoritable
+
+  def allergens
+    allergens = "アーモンド・あわび・いか・いくら・オレンジ・カシューナッツ・キウイフルーツ・"\
+                "牛肉・くるみ・ごま・さけ・さば・大豆・鶏肉・バナナ・豚肉・まつたけ・もも・やまいも・りんご・"\
+                "ゼラチン・えび・かに・小麦・そば・卵・乳・落花生・ピーナッツ".split('・')
+    allergens.select { |allergen| ingredients.include?(allergen) }
+  end
 end
