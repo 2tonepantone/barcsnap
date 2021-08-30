f5f467bba3b2a60f404cf84df604847c27284223
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-30 18:54:44 +0900

    move allergens into a constant

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  ALLERGENS = %w[アーモンド あわび いか いくら オレンジ カシューナッツ キウイフルーツ
+                 牛肉 くるみ ごま さけ さば 大豆 鶏肉 バナナ 豚肉 まつたけ もも やまいも りんご
+                 ゼラチン えび かに 小麦 そば 卵 乳 落花生 ピーナッツ]
   has_one_attached :photo
   has_many :reviews
 
@@ -10,10 +13,7 @@ class Product < ApplicationRecord
   acts_as_favoritable
 
   def allergens
-    allergens = "アーモンド・あわび・いか・いくら・オレンジ・カシューナッツ・キウイフルーツ・"\
-                "牛肉・くるみ・ごま・さけ・さば・大豆・鶏肉・バナナ・豚肉・まつたけ・もも・やまいも・りんご・"\
-                "ゼラチン・えび・かに・小麦・そば・卵・乳・落花生・ピーナッツ".split('・')
-    contains = allergens.select { |allergen| ingredients.include?(allergen) }.join(', ') if ingredients
+    contains = ALLERGENS.select { |allergen| ingredients.include?(allergen) }.join(', ') if ingredients
     return "No ingredients provided. For your own safety, please check the product thoroughly." if contains.nil?
     return "No allergens found. For your own safety, please check the product thoroughly." if contains.empty?
 
