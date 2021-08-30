df96b2dd9f2c183db4521914a52c4a9ee8dc655e
kandanai leenutaphong <mew.chayutaphong@gmail.com>
2021-08-30 20:48:42 +0700

    write a function to get all the categories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,33 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :ui_kits ]
 
   def home
-    @top_rating_products = Product.all.sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
+    categories
+    # @top_rating_products = Product.all.sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
+    @top_rating_products = Product.all.sort_by { |p| p.avg_rating || 0 }
+    categorySort(@top_rating_products, "スナック菓子")
     @most_liked_products = Product.all.sort_by { |p| p.favoritors.count }.reverse[0..5]
+    raise
+  end
+
+  private
+  def categorySort(lists, category)
+    answer = []
+    lists.each do |list|
+      if list.tag_list.last == category
+        answer << list
+      end
+    end
+    return answer
+  end
+
+  def categories
+    array = []
+    Product.all.each do |product|
+      category = product.tag_list.last
+      unless array.include?(category)
+        array << category
+      end
+    end
+    return array
   end
 end
