52027c14020430ec55a4e558433899c4ba6f3a22
kandanai leenutaphong <mew.chayutaphong@gmail.com>
2021-08-30 22:41:02 +0700

    draft

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,23 +3,39 @@ class PagesController < ApplicationController
 
   def home
     categories
-    # @top_rating_products = Product.all.sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
-    @top_rating_products = Product.all.sort_by { |p| p.avg_rating || 0 }
-    snacks = categorySort(@top_rating_products, "スナック菓子")
-    hair_care_styling = categorySort(@top_rating_products, "ヘアケア・スタイリング")
-    shampoo = categorySort(@top_rating_products, "シャンプー")
-    baby_powder = categorySort(@top_rating_products, "ベビーパウダー")
-    sweets_and_sweets = categorySort(@top_rating_products, "スイーツ・お菓子")
-    water_soft_drink = categorySort(@top_rating_products, "水・ソフトドリンク")
-    computer_peripherals = categorySort(@top_rating_products, "パソコン・周辺機器")
-    soup = categorySort(@top_rating_products, "スープ")
-    oa_cleaner = categorySort(@top_rating_products, "oaクリーナー")
-    sports_drink =categorySort(@top_rating_products,"スポーツドリンク")
+    results = ActsAsTaggableOn::Tag.most_used(10)
+    first_category_products = Product.tagged_with(results[0].name)
 
 
+    @top_rating_products = Product.all.sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
+    # @most_liked_products = Product.all.sort_by { |p| p.favoritors.count }.reverse[0..5]
+    @first_rating_products = Product.tagged_with(results[0].name).sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
+    @second_rating_products = Product.tagged_with(results[1].name).sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
+    @third_rating_products = Product.tagged_with(results[2].name).sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
+    @fourth_rating_products = Product.tagged_with(results[3].name).sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
+    # raise
+    @top_rating_snacks = Product.tagged_with("スナック菓子").sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
+    @top_rating_shampoo = Product.tagged_with("ヘアケア・スタイリング").sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
+    # @top_rating_products = Product.all.sort_by { |p| p.avg_rating || 0 }
+    # snacks = categorySort(@top_rating_products, "スナック菓子")
+    # hair_care_styling = categorySort(@top_rating_products, "ヘアケア・スタイリング")
+    # shampoo = categorySort(@top_rating_products, "ヘアケア・スタイリング")
+    # baby_powder = categorySort(@top_rating_products, "ベビーパウダー")
+    # sweets_and_sweets = categorySort(@top_rating_products, "スイーツ・お菓子")
+    # water_soft_drink = categorySort(@top_rating_products, "水・ソフトドリンク")
+    # computer_peripherals = categorySort(@top_rating_products, "パソコン・周辺機器")
+    # soup = categorySort(@top_rating_products, "スープ")
+    # oa_cleaner = categorySort(@top_rating_products, "oaクリーナー")
+    # sports_drink =categorySort(@top_rating_products,"スポーツドリンク")
 
-    @most_liked_products = Product.all.sort_by { |p| p.favoritors.count }.reverse[0..5]
-    raise
+    # count = []
+    # categories.each do |category|
+    #   count << categorySort(@top_rating_products, category).count
+    # end
+
+    # raise
+
+
   end
 
   private
