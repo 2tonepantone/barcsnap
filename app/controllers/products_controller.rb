495697cc3498f6e394e97ceb01c62a6d3ee3f1da
kochapas <kochapas@outlook.com>
2021-08-30 14:17:31 +0900

    Improve CTA's size/style, Fix product card hover effect, Remove price from product cards, Add rating to product cards, Fix product cards responsiveness, Fix banner typo, Remove unwanted file.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -89,11 +89,7 @@ class ProductsController < ApplicationController
     when "most_favorite"
       @products = Product.all.sort_by { |p| p.favoritors.count }.reverse
     when "top_rating"
-      @products = Product.all.sort_by do |p|
-        reviews = p.reviews
-        rating = reviews.count.positive? ? (reviews.map(&:rating).sum / reviews.count).round(2) : 0
-        rating
-      end.reverse
+      @products = Product.all.sort_by(&:avg_rating_str).reverse
     when "newest"
       @products = Product.order(created_at: :desc)
     else
@@ -109,11 +105,7 @@ class ProductsController < ApplicationController
     when "most_favorite"
       @products = product.find_related_on_tags.sort_by { |p| p.favoritors.count }.reverse
     when "top_rating"
-      @products = product.find_related_on_tags.sort_by do |p|
-        reviews = p.reviews
-        rating = reviews.count.positive? ? (reviews.map(&:rating).sum / reviews.count).round(2) : 0
-        rating
-      end.reverse
+      @products = product.find_related_on_tags.sort_by(&:avg_rating_str).reverse
     when "newest"
       @products = product.find_related_on_tags.sort_by(&:created_at).reverse
     else
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,11 @@ class Product < ApplicationRecord
   acts_as_taggable_on :tags
   ActsAsTaggableOn.force_lowercase = true
   acts_as_favoritable
+
+  def avg_rating_str
+    avg = "-"
+    reviews = self.reviews
+    avg = (reviews.map(&:rating).sum / reviews.count).round(2) if reviews.count.positive?
+    return avg.to_s
+  end
 end
