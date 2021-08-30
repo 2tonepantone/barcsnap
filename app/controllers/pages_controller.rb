589685388951028fcab2ade229dac97c4e9d1c30
kandanai leenutaphong <mew.chayutaphong@gmail.com>
2021-08-30 21:01:34 +0700

    writing out all the categories in array

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,19 @@ class PagesController < ApplicationController
     categories
     # @top_rating_products = Product.all.sort_by { |p| p.avg_rating || 0 }.reverse[0..5]
     @top_rating_products = Product.all.sort_by { |p| p.avg_rating || 0 }
-    categorySort(@top_rating_products, "スナック菓子")
+    snacks = categorySort(@top_rating_products, "スナック菓子")
+    hair_care_styling = categorySort(@top_rating_products, "ヘアケア・スタイリング")
+    shampoo = categorySort(@top_rating_products, "シャンプー")
+    baby_powder = categorySort(@top_rating_products, "ベビーパウダー")
+    sweets_and_sweets = categorySort(@top_rating_products, "スイーツ・お菓子")
+    water_soft_drink = categorySort(@top_rating_products, "水・ソフトドリンク")
+    computer_peripherals = categorySort(@top_rating_products, "パソコン・周辺機器")
+    soup = categorySort(@top_rating_products, "スープ")
+    oa_cleaner = categorySort(@top_rating_products, "oaクリーナー")
+    sports_drink =categorySort(@top_rating_products,"スポーツドリンク")
+
+
+
     @most_liked_products = Product.all.sort_by { |p| p.favoritors.count }.reverse[0..5]
     raise
   end
