863734afe9dbc03772a7d810ca6ebfbbe163012a
kandanai leenutaphong <mew.chayutaphong@gmail.com>
2021-08-31 08:08:19 +0700

    barcsnap as default value in form

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,9 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :rating, :inclusion => 1..5
+  after_initialize :init
+
+  def init
+    self.source  ||= "barcsnap.com"           #will set the default value only if it's nil
+  end
 end
