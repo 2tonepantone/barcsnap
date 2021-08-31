847fabcb0283a93dcf331424b6d71ae588659b92
kandanai leenutaphong <mew.chayutaphong@gmail.com>
2021-08-31 18:59:46 +0700

    add allergens column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_031312) do
+ActiveRecord::Schema.define(version: 2021_08_31_114822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
