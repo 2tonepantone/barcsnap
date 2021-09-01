fe3c48ca4b3461cd073ac5ea451f25ab002a8564
kandanai leenutaphong <mew.chayutaphong@gmail.com>
2021-09-01 16:41:25 +0700

    fix spinner not spinning

--- app/javascript/plugins/scanditsdk.js
+++ app/javascript/plugins/scanditsdk.js
@@ -1,6 +1,7 @@
 import * as ScanditSDK from "scandit-sdk";
 import { SingleImageModeSettings } from "scandit-sdk";
 import { Spinner } from 'spin.js';
+import 'spin.js/spin.css';
 
 const initScanditSDK = () => {
   ScanditSDK.configure("AelgfmP2Q/QqARqoCjtBkSsu6zcXPYf4bWyk7QUI+1CzX7wP7FMwArwsTKGIfFWCRHCcxgcNfk8OWtyhsWLDbj4BK8AGfNlBiBpWvnZsN2kvTdI0nG4yC0sH+75wCeBZdDR1iq8Y7xR+/Ob7EeB39dLgYbRgIcV2FO17/2knl+lxRSq+LdKwAaDhdKPn8/CXRpZiuwxJvmhOFlVO1zNxnVgoRFy6ifQbQNDbmIo8O+3ReKdEVld5qqdo0MqD2fC3nN93WGxRJHQOl1LmglKYLJ7ofITQtyS2nXLOsjPRkK/+xpYPQK33TWDHD9qzkkKhAjeVj0+MRT5y0FOp4g0thVBQ0RwiGGWVr5dGfFGvsYyUHYr+hPUwWL8HDlxmMrwVcVrPIIdXnnlTPICPkNAmd9WG8/gAGBCUNJCPpvgXCJJeGgJW/Yv96p7m21KpuInbvtSczQGzMJDUjNekPHv0N+zuO45aU5TNQI66wPpdXq8t6vs7S0tr8mDP8Hoe8SSgUDWphHj/+kP8E/WDOCMsZmKqkRuzrQa3JR7M6goUoCuyfCtQQKB83em5L5teMVcEvW9EduGEykfcpOW6uVl3OQKLf3fEikFlayV6Cs7sRVwoDYUxtreq1aEcRBWJ5088AzBpCTNWdBBFNreGlInw+6Hl8OWk8sGfgxOPaGIPWmdOqBIN/ca+/Jvf+xLPaH2sm28/Fg6trwOZSyUxlFBm0ZrZjlSBzRCqWZGlpJew+JNIDlg0hk+v5aid/NrWfy7aM++XDqCXWk69k+uNZFa9A7li46x6iNa2UqA49T+SJ+OxkcloYnbGUg==", {
@@ -74,11 +75,11 @@ const initScanditSDK = () => {
           position: 'absolute',
         }
 
-        var load_screen = document.getElementById("load_screen");
-        var spinner = new Spinner(opts).spin(load_screen);
-        load_screen.appendChild(spinner.el);
+        const load_screen = document.getElementById("spinner");
+        new Spinner(opts).spin(load_screen);
+        // load_screen.appendChild(spinner.el);
         // document.body.style.background = "black";
-        debugger
+        // debugger
         let barcode = scanResult.barcodes[0].data;
         const input = document.getElementById('barcodeInput');
         input.setAttribute('value', barcode);
