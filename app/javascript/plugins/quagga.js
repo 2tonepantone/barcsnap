016254734a40457462efcc38f269894c8fd75b73
Kyle Bailey <kylealexbailey@gmail.com>
2021-10-28 18:23:15 +0900

    filter out barcodes read in error

--- app/javascript/plugins/quagga.js
+++ app/javascript/plugins/quagga.js
@@ -7,7 +7,7 @@ Quagga.init({
     target: document.querySelector('#barcode-scanner')
   },
   decoder: {
-    readers: ['ean_reader', 'ean_8_reader']
+    readers: ['ean_reader']
   }
 }, function (err) {
   if (err) {
@@ -17,7 +17,43 @@ Quagga.init({
   console.log("Initialization finished. Ready to start");
   Quagga.start();
 
+  let codes = [];
   Quagga.onDetected(function (data) {
-    console.log(data.codeResult.code);
+    codes.push(data.codeResult.code);
+    console.log(codes.length);
+    if (codes.length >= 15) {
+      const barcode = mostFrequent(codes, codes.length);
+      console.log(barcode);
+      codes = [];
+    }
   });
 });
+
+function mostFrequent(arr, n) {
+  // Sort the array
+  arr.sort();
+
+  // find the max frequency using linear
+  // traversal
+  let max_count = 1, res = arr[0];
+  let curr_count = 1;
+
+  for (let i = 1; i < n; i++) {
+    if (arr[i] == arr[i - 1])
+      curr_count++;
+    else {
+      if (curr_count > max_count) {
+        max_count = curr_count;
+        res = arr[i - 1];
+      }
+      curr_count = 1;
+    }
+  }
+
+  // If last element is most frequent
+  if (curr_count > max_count) {
+    max_count = curr_count;
+    res = arr[n - 1];
+  }
+  return res;
+}
